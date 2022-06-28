acd8c6ee1220c5c13f398224f09e31e86018569f
Ariane Barbosa <abarbosa.ipms@gmail.com>
2022-06-27 22:04:29 -0300

    Desafios realizados

---
+++ desafio1.ts
@@ -0,0 +1,16 @@
+/*
+Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+let employee = {};
+employee.code = 10;
+employee.name = "John";
+
+*/
+
+const employee = {
+    code: 10,
+    name: "John"
+}
+
+employee.code;
+employee.name;
